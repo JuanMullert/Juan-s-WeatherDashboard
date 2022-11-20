2d72b53c586001278a8688d321aad7402ae8af72
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2022-11-20 14:36:10 -0600

    created function

--- assets/script.js
+++ assets/script.js
@@ -47,3 +47,78 @@ var getCityWeather = function (searchCityName) {
   });
 };
 
+var getUVIndex = function (lat, lon) {
+    // formate the OpenWeather api url
+    var apiUrl =
+      dailyUVIndexApiStarts +
+      personalAPIKey +
+      "&lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&" +
+      unit;
+    fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        // remove all class background
+        $("#UVIndexToday").removeClass();
+        $("#UVIndexToday").html(response.value);
+        if (response.value < 3) {
+          $("#UVIndexToday").addClass("p-1 rounded bg-success text-white");
+        } else if (response.value < 8) {
+          $("#UVIndexToday").addClass("p-1 rounded bg-warning text-white");
+        } else {
+          $("#UVIndexToday").addClass("p-1 rounded bg-danger text-white");
+        }
+      });
+  };
+  var getForecast = function (lat, lon) {
+    // formate the OpenWeather api url
+    var apiUrl =
+      forecastWeatherApiStarts +
+      "lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&exclude=current,minutely,hourly" +
+      "&" +
+      personalAPIKey +
+      "&" +
+      unit;
+    fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        for (var i = 1; i < 6; i++) {
+          //display date
+          var unixTime = response.daily[i].dt;
+          var date = moment.unix(unixTime).format("MM/DD/YY");
+          $("#Date" + i).html(date);
+          // display weather icon
+          var weatherIncoUrl =
+            "http://openweathermap.org/img/wn/" +
+            response.daily[i].weather[0].icon +
+            "@2x.png";
+          $("#weatherIconDay" + i).attr("src", weatherIncoUrl);
+          // display temperature
+          var temp = response.daily[i].temp.day + " \u00B0F";
+          $("#tempDay" + i).html(temp);
+          // display humidity
+          var humidity = response.daily[i].humidity;
+          $("#humidityDay" + i).html(humidity + " %");
+        }
+      });
+  };
+  //-------------------------- get weather info from OpenWeather ends here ------------------------------//
+  //-------------------------------------- create button starts  ----------------------------------------//
+  var creatBtn = function (btnText) {
+    var btn = $("<button>")
+      .text(btnText)
+      .addClass("list-group-item list-group-item-action")
+      .attr("type", "submit");
+    return btn;
+  };
