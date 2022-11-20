366653438d379b69ca7d8c658e534ac2e5d9e00b
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2022-11-19 20:24:47 -0600

    declared variables

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// set global variables
+var citiesListArr = [];
+var numOfCities = 9;
+var personalAPIKey = "appid=264dbcaa3899de05eeadc78d68ba06dc";
+var unit = "units=imperial";
+var dailyWeatherApiStarts =
+  "https://api.openweathermap.org/data/2.5/weather?q=";
+var dailyUVIndexApiStarts = "https://api.openweathermap.org/data/2.5/uvi?";
+var forecastWeatherApiStarts =
+  "https://api.openweathermap.org/data/2.5/onecall?";
+
+  
