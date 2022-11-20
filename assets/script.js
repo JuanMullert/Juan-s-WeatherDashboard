e64f70e2872faaa199281b622b8c33728f349dd9
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2022-11-20 15:05:58 -0600

    added button and called functions

--- assets/script.js
+++ assets/script.js
@@ -161,3 +161,61 @@ var loadSavedCity = function () {
     newcity = 1;
     return newcity;
   };
+
+//------------------------ save searched city in to local storage ends here ---------------------------//
+//-------------------------- create button with searched city starts here -----------------------------//
+var createCityNameBtn = function (searchCityName) {
+  var saveCities = JSON.parse(localStorage.getItem("weatherInfo"));
+  // check the searchCityName parameter against all children of citiesListArr
+  if (saveCities.length == 1) {
+    var cityNameBtn = creatBtn(searchCityName);
+    searchedCities.prepend(cityNameBtn);
+  } else {
+    for (var i = 1; i < saveCities.length; i++) {
+      if (searchCityName.toLowerCase() == saveCities[i].toLowerCase()) {
+        return;
+      }
+    }
+    // check whether there are already have too many elements in this list of button
+    if (searchedCities[0].childElementCount < numOfCities) {
+      var cityNameBtn = creatBtn(searchCityName);
+    } else {
+      searchedCities[0].removeChild(searchedCities[0].lastChild);
+      var cityNameBtn = creatBtn(searchCityName);
+    }
+    searchedCities.prepend(cityNameBtn);
+    $(":button.list-group-item-action").on("click", function () {
+      BtnClickHandler(event);
+    });
+  }
+};
+
+//-------------- call functions directly --------------------//
+loadSavedCity();
+//-- create button with searched city ends here --//
+//--- event handler from submit form starts here ---//
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  // name of the city
+  var searchCityName = $("#searchCity").val().trim();
+  var newcity = saveCityName(searchCityName);
+  getCityWeather(searchCityName);
+  if (newcity == 1) {
+    createCityNameBtn(searchCityName);
+  }
+};
+var BtnClickHandler = function (event) {
+  event.preventDefault();
+  // name of the city
+  var searchCityName = event.target.textContent.trim();
+  getCityWeather(searchCityName);
+};
+//--------------------------- event handler from submit form ends here ------------------------------//
+//------------------------ call functions with submit button starts here ----------------------------//
+$("#searchCityForm").on("submit", function () {
+  formSubmitHandler(event);
+});
+$(":button.list-group-item-action").on("click", function () {
+  BtnClickHandler(event);
+});
+//-------------------------- call functions with submit button ends here ----------------------------//
